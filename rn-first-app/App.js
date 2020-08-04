20985dfcc0f2f27882ad10767f52b815204072c8
Ryan <rydawnshootingstar@gmail.com>
2020-08-04 16:20:22 -0400

    pho bg header, Button

--- rn-first-app/App.js
+++ rn-first-app/App.js
@@ -1,21 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { SafeAreaView, Text, Button } from "react-native";
+import styled from "styled-components";
+import { AntDesign } from "@expo/vector-icons";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <Container>
+            <StatusBar barStyle='light-content' />
+            <PhoBackground source={require("./assets/pho.png")}>
+                <SafeAreaView>
+                    <MenuBar>
+                        <Back>
+                            <AntDesign
+                                name='arrowleft'
+                                size={30}
+                                color='#000'
+                            />
+                            <Text style={{ marginLeft: 10, color: "#000" }}>
+                                Back
+                            </Text>
+                        </Back>
+                    </MenuBar>
+                </SafeAreaView>
+            </PhoBackground>
+            <TitleText>Pho To Go</TitleText>
+
+			<OrderButton title="Order Now" ></OrderButton>
+        </Container>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Container = styled.View`
+    display: flex;
+    height: 100%;
+    margin-top: 10%;
+`;
+
+const TitleText = styled.Text`
+    font-size: 25px;
+    text-align: center;
+`;
+
+const PhoBackground = styled.ImageBackground`
+    width: 100%;
+    height: 75px;
+`;
+
+const MenuBar = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 16px;
+`;
+
+const Back = styled.View`
+    flex-direction: row;
+    align-items: center;
+`;
+
+const OrderButton = styled.Button`
+max-width: 30px;
+overflow: hidden;
+background-color: red;
+`
